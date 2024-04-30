e6a1ce18f04b5cd66d78033b8954d0022e4f0fe9
rebecalazarini <160655787+rebecalazarini@users.noreply.github.com>
2024-04-30 15:27:58 -0300

    Create construtor.bat

---
+++ construtor.bat
@@ -0,0 +1,13 @@
+md programa
+cd programa
+md paginas
+cd paginas
+type nul > page1.html
+type nul > page2.html
+type nul > page3.html
+cd ..
+md estilos
+cd estilos
+type nul > style1.css
+type nul > style2.css
+type nul > style3.css
