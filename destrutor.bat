2c4617cdb5be752f1a1752c38052268c9142ba3a
rebecalazarini <160655787+rebecalazarini@users.noreply.github.com>
2024-04-30 15:28:28 -0300

    Create destrutor.bat

---
+++ destrutor.bat
@@ -0,0 +1,15 @@
+cd programa
+cd paginas
+del page1.html
+del page2.html
+del page3.html
+cd ..
+cd estilos
+del style1.css
+del style2.css
+del style3.css
+cd ..
+rd estilos
+rd paginas
+cd ..
+rd programa
